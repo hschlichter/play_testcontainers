05e5f6300cc2ca5794fa9d5baf3b4ebdb664572e
Henrik Schlichter <henrik.schlichter@gmail.com>
2024-05-14 11:36:08 +0200

    format fix

--- tests/integration_valkey.rs
+++ tests/integration_valkey.rs
@@ -1,13 +1,11 @@
 use play_testcontainers::PlayRedis;
-use testcontainers::{runners::AsyncRunner, GenericImage, core::WaitFor};
+use testcontainers::{core::WaitFor, runners::AsyncRunner, GenericImage};
 
 #[tokio::test]
 async fn test_redis_async() {
     let container = GenericImage::new("valkey/valkey", "7.2.5")
         .with_exposed_port(6379)
-        .with_wait_for(WaitFor::message_on_stdout(
-            "Ready to accept connections",
-        ))
+        .with_wait_for(WaitFor::message_on_stdout("Ready to accept connections"))
         .start()
         .await;
 
